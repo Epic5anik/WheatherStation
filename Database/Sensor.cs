4efb0278cc9cc6805aaabc1bcca0b043cde8583b
uczen <uczen@EDWARD>
2025-11-21 15:49:40 +0100

    15:49 21.11

--- Database/Sensor.cs
+++ Database/Sensor.cs
@@ -7,4 +7,12 @@ public class Sensor
     public string Name { get; set; }
 
     public DateTime MeasurementDate { get; set; }
+
+    private static int counter;
+
+    public Sensor()
+    {
+        counter++;
+    }
+    
 }
--- Database/User.cs
+++ Database/User.cs
@@ -1,8 +1,24 @@
+using System.Diagnostics.Metrics;
+
 namespace Database;
 
 public class User
 {
     public int Id { get; set; }
     public string Name { get; set; }
+
+
+
+    private static int counter;
+
+
+
+    public User(string name, int id)
+    {
+        Name = name;
+        Id = id;
+
+        counter++;
+    }
 
 }
