6e2898cc2d7aa0547c9050cecafb0894fca90a3b
uczen <uczen@TOMASZ>
2026-02-27 15:50:21 +0100

    .rtrt.

---
+++ Repozitory/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repozitory
+{
+    internal interface IUserRepository
+    {
+        User? GetUser(int id);
+    }
+}
---
+++ Repozitory/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repozitory
+{
+    internal class UserRepository : IUserRepository
+    {
+        private readonly MyApplycationContekst _dbContext;
+
+        public UserRepository(MyApplycationContekst dbContext)
+        {   
+            _dbContext = dbContext;
+        }
+
+        public User? GetUser(int id)   
+        {
+            return _dbContext.Set<User>().FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
