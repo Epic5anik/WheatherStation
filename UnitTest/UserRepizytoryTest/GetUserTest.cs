bb6e7c7bdd97ce3d3a4692d61d8abe43cd2a7d39
uczen <uczen@EDWARD>
2026-04-24 15:56:56 +0200

    fff 24

---
+++ UnitTest/UserRepizytoryTest/GetUserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.UserRepizytoryTest
+{
+    public class GetUserTest
+    {
+        [Fact]
+        public void GetUser_ReturnsUserWithSuccess_WhenUserExistsInDatabase()
+        {
+            /// 
+            var userRepository = new UserRepository();
+        
+        }
+        [Fact]
+        public void GetUser_ReturnsNull_WhenUserDoesNotExistInDatabase()
+        {
+            ///
+
+        }
+
+    }
+}
